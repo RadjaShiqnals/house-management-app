872678cc8faedf39cab8ffe9bde1a4f90a9f3bac
RadjaShiqnals <radjagenta@gmail.com>
2026-04-28 10:09:33 +0700

    first commit

---
+++ backend/database/migrations/2026_04_28_023655_create_residents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('residents', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('id_card_path');
+            $table->enum('status', ['permanent', 'contract']);
+            $table->string('phone_number');
+            $table->boolean('is_married');
+            $table->foreignId('house_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('residents');
+    }
+};
